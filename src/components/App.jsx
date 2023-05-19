4354c114a8939d6e323d5b292dd7bfb3d2188229
Dmytro-Kobets <kobets.dmytro9@gmail.com>
2023-05-19 12:13:12 +0300

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,12 +6,7 @@ import { Wrapper } from './App.styled';
 import { useState } from 'react';
 
 export const App = () => {
-  // state = {
-  //   good: 0,
-  //   neutral: 0,
-  //   bad: 0,
-  // };
-
+  
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
