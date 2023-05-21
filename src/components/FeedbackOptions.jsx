d8cd6c293e9d4957c0354ce697ff91b98adf82d6
Dmytro-Kobets <kobets.dmytro9@gmail.com>
2023-05-21 13:27:36 +0300

    fix

--- src/components/FeedbackOptions.jsx
+++ src/components/FeedbackOptions.jsx
@@ -1,21 +1,18 @@
-import { Component } from 'react';
 import { OptionButton } from './App.styled';
 import PropTypes from 'prop-types';
 
-export class FeedbackOptions extends Component {
-  render() {
-    const { good, setGood, neutral, setNeutral, bad, setBad } = this.props;
-    return (
-      <>
-        <OptionButton onClick={() => setGood(good + 1)}>Good</OptionButton>
-        <OptionButton onClick={() => setNeutral(neutral + 1)}>
-          Neutral
-        </OptionButton>
-        <OptionButton onClick={() => setBad(bad + 1)}>Bad</OptionButton>
-      </>
-    );
-  }
-}
+export const FeedbackOptions = () => {
+  const { good, setGood, neutral, setNeutral, bad, setBad } = this.props;
+  return (
+    <>
+      <OptionButton onClick={() => setGood(good + 1)}>Good</OptionButton>
+      <OptionButton onClick={() => setNeutral(neutral + 1)}>
+        Neutral
+      </OptionButton>
+      <OptionButton onClick={() => setBad(bad + 1)}>Bad</OptionButton>
+    </>
+  );
+};
 
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string.isRequired),
